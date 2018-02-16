d5cbc84f3b5fa259a06440bdd31aa3f503497d3d
micael-grilo <micaelf@outlook.com>
2018-02-16 11:08:40 +0000

    v0.5.0 release

--- gouvpt/__init__.py
+++ gouvpt/__init__.py
@@ -4,7 +4,7 @@ Official udata theme and extensions of the Open Data Portal of Portugal
 '''
 from __future__ import unicode_literals
 
-__version__ = '0.1.0.dev'
+__version__ = '0.5.0'
 __description__ = 'Official udata theme and extensions of the Open Data Portal of Portugal'
 
 def init_app(app):
