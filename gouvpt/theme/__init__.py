ede0af433fdeeb77af5e7a75cb8cfc2ae6d7f372
micael-grilo <micaelf@outlook.com>
2018-03-05 17:15:24 +0000

    #Feature Theme0.9 - Initial theme changes

--- gouvpt/theme/__init__.py
+++ gouvpt/theme/__init__.py
@@ -17,55 +17,27 @@ log = logging.getLogger(__name__)
 
 gouvpt_menu = nav.Bar('gouvpt_menu', [
 
-    nav.Item(_('Open Data'), 'gouvpt.faq', items=[
-        nav.Item(_('Citizens'), 'gouvpt.faq', {'section': 'citizen'}),
-        nav.Item(_('Producers'), 'gouvpt.faq', {'section': 'producer'}),
-        nav.Item(_('Reusers'), 'gouvpt.faq', {'section': 'reuser'}),
-        nav.Item(_('Integrator'), 'gouvpt.faq', {'section': 'integrator'}),
-        nav.Item(_('Developers'), 'gouvpt.faq', {'section': 'developer'}),
+    nav.Item(_('Start'),  None, url='#', items=[
+        nav.Item((_('About')+' '+_('Open Data')), 'gouvpt.faq', {'section': 'about_opendata'}),
+        nav.Item((_('About')+' Dados.gov'), 'gouvpt.faq', {'section': 'about_dadosgov'}),
+        nav.Item(_('Publish data'), 'gouvpt.faq', {'section': 'publish'}),
+        nav.Item((_('Reuse')+' '+_('Data')), 'gouvpt.faq', {'section': 'reuse'}),
+        nav.Item(_('Licenses'), 'gouvpt.faq', {'section': 'licenses'}),
     ]),
 
-    nav.Item(_('Resources'), 'datasets.list', items=[
+    nav.Item(_('Open Data'), None, url='#', items=[
         nav.Item(_('Datasets'), 'datasets.list'),
         nav.Item(_('Reuses'), 'reuses.list'),
         nav.Item(_('Organizations'), 'organizations.list'),
-        nav.Item(_('API'), 'apidoc.swaggerui'),
-        nav.Item(_('Terms of use'), 'site.terms'),
         nav.Item(_('Dashboard'), 'site.dashboard'),
     ]),
 
-    nav.Item(_('Blog/Events'), None, url='#', items=[
+    nav.Item(_('News'), None, url='#', items=[
         nav.Item(_('News'), 'posts.list'),
+        nav.Item(_('Events'), 'posts.list', {'tag': 'events'}),
     ]),
 
-    nav.Item(_('Contact'), None, url='#'),
+    nav.Item(_('Contact'), 'gouvpt.contact'),
 ])
 
 theme.menu(gouvpt_menu)
-
-nav.Bar('gouvpt_footer', [
-
-    nav.Item(_('Open Data'), 'gouvpt.faq', items=[
-        nav.Item(_('Citizens'), 'gouvpt.faq', {'section': 'citizen'}),
-        nav.Item(_('Producers'), 'gouvpt.faq', {'section': 'producer'}),
-        nav.Item(_('Reusers'), 'gouvpt.faq', {'section': 'reuser'}),
-        nav.Item(_('Integrator'), 'gouvpt.faq', {'section': 'integrator'}),
-        nav.Item(_('Developers'), 'gouvpt.faq', {'section': 'developer'}),
-    ]),
-
-    nav.Item(_('Resources'), 'datasets.list', items=[
-        nav.Item(_('Datasets'), 'datasets.list'),
-        nav.Item(_('Reuses'), 'reuses.list'),
-        nav.Item(_('Organizations'), 'organizations.list'),
-        nav.Item(_('API'), 'apidoc.swaggerui'),
-        nav.Item(_('Terms of use'), 'site.terms'),
-        nav.Item(_('Dashboard'), 'site.dashboard'),
-    ]),
-
-    nav.Item(_('Blog'), None, url='#', items=[
-        nav.Item(_('News'), 'posts.list'),
-        nav.Item(_('Events'), None, url='http://www.dados.gov.pt'),
-    ]),
-
-    nav.Item(_('Dashboard'), 'site.dashboard'),
-])
