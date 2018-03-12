fa52887654fd8d3f0bccdb6b1dc68ebd13119024
micael-grilo <micaelf@outlook.com>
2018-03-12 13:07:46 +0000

    Theme v0.9 - HTML/CSS changes

--- gouvpt/theme/__init__.py
+++ gouvpt/theme/__init__.py
@@ -17,7 +17,7 @@ log = logging.getLogger(__name__)
 
 gouvpt_menu = nav.Bar('gouvpt_menu', [
 
-    nav.Item(_('Init'),  None, url='#', items=[
+    nav.Item(_('Documentation'),  None, url='#', items=[
         nav.Item(_('About Open Data'), 'gouvpt.faq', {'section': 'about_opendata'}),
         nav.Item(_('About Dados.gov'), 'gouvpt.faq', {'section': 'about_dadosgov'}),
         nav.Item(_('Publish data'), 'gouvpt.faq', {'section': 'publish'}),
