ba07693d42b6a82f97a19dc726c3f1607460f9da
Micael Grilo <micaelf@outlook.com>
2019-06-25 23:31:42 +0100

    Fix filename at conversion and dataset name

--- gouvpt/theme/static/js/datasets.js
+++ gouvpt/theme/static/js/datasets.js
@@ -145,7 +145,7 @@ function download(data, filename, type) {
 function exportFile(format) {
     var selectBox = document.getElementById("select-resource");
     var url = selectBox.options[selectBox.selectedIndex].value;
-    var filename = selectBox.options[selectBox.selectedIndex].label;
+    var filename = (selectBox.options[selectBox.selectedIndex].label).replace(/\.[^/.]+$/, "");
 
     if (!format) {
         window.location.href = url;
