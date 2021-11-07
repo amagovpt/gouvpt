8337bd517a2faa3987a2722b9d6a3edc255935fc
Micael Grilo <micaelf@outlook.com>
2021-11-07 22:12:52 +0000

    Update translations

--- gouvpt/theme/__init__.py
+++ gouvpt/theme/__init__.py
@@ -18,21 +18,21 @@ log = logging.getLogger(__name__)
 
 gouvpt_menu = nav.Bar('gouvpt_menu', [
 
-    nav.Item(_('Documentation'),  None, url='#', items=[
-        nav.Item(_('About Open Data'), 'gouvpt.faq', {'section': 'about_opendata'}),
-        nav.Item(_('About Dados.gov'), 'gouvpt.faq', {'section': 'about_dadosgov'}),
-        nav.Item(_('Publish data'), 'gouvpt.faq', {'section': 'publish'}),
-        nav.Item(_('Reuse data'), 'gouvpt.faq', {'section': 'reuse'}),
+    nav.Item(_('Documentação'),  None, url='#', items=[
+        nav.Item(_('Sobre dados abertos'), 'gouvpt.faq', {'section': 'about_opendata'}),
+        nav.Item(_('Sobre o dados.gov'), 'gouvpt.faq', {'section': 'about_dadosgov'}),
+        nav.Item(_('Publicar dados'), 'gouvpt.faq', {'section': 'publish'}),
+        nav.Item(_('Reutilizar dados'), 'gouvpt.faq', {'section': 'reuse'}),
         nav.Item(_('Licenses'), 'gouvpt.faq', {'section': 'licenses'}),
         nav.Item(_('Acessibilidade'), 'gouvpt.faq', {'section': 'acessibilidade'}),
         nav.Item(_('API'), 'gouvpt.docapi'),
     ]),
 
-    nav.Item(_('Open Data'), None, url='#', items=[
+    nav.Item(_('Dados Abertos'), None, url='#', items=[
         nav.Item(_('Datasets'), 'datasets.list'),
         #nav.Item(_('Data Analysis'), 'gouvpt.rawgraphs', {'embed': 'true'}),
         nav.Item(_('Reuses'), 'reuses.list'),
-        nav.Item(_('Visualizations'), 'reuses.list', {'type': 'visualization'}),
+        nav.Item(_('Visualizações'), 'reuses.list', {'type': 'visualization'}),
         nav.Item(_('Organizations'), 'organizations.list'),
         nav.Item(_('Dashboard'), 'site.dashboard'),
         nav.Item(_('Map'), 'site.map'),
@@ -40,7 +40,7 @@ gouvpt_menu = nav.Bar('gouvpt_menu', [
 
     nav.Item(_('News'), None, url='#', items=[
         nav.Item(_('News'), 'posts.list'),
-        nav.Item(_('Events'), 'posts.list', {'tag': 'evento'}),
+        nav.Item(_('Eventos'), 'posts.list', {'tag': 'evento'}),
     ]),
 
     nav.Item(_('Contact'), 'gouvpt.contact'),
